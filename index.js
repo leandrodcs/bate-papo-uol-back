b6d6c18a9c1e437b3d9697ff95ec065ddc978f92
leandrodcs <eng.leandrodcs@gmail.com>
2021-10-10 16:16:38 -0300

    Get function added to messages route

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ const participants = [
         lastStatus: Date.now()
     }
 ];
-const messages = [];
+const messages = [
+    {
+        from: "Maria",
+        to: "lester",
+        text: "teste 1",
+        type: "message",
+        time: dayjs().format('hh:mm:ss')
+    },
+    {
+        from: "Maria",
+        to: "lester",
+        text: "teste 2",
+        type: "message",
+        time: dayjs().format('hh:mm:ss')
+    },
+        {
+        from: "Maria",
+        to: "lester",
+        text: "teste 3",
+        type: "message",
+        time: dayjs().format('hh:mm:ss')
+    },
+];
 
 server.post(`/participants`, (req, res) => {
     const newUser = {
@@ -44,14 +66,27 @@ server.get(`/participants`, (req, res) => {
 server.post(`/messages`, (req, res) => {
     const newMessage = {
         from: req.headers.user,
-        ...req.body
+        ...req.body,
+        time: dayjs().format('hh:mm:ss')
     };
     if(!newMessage.to || !newMessage.text || (newMessage.type !== "message" && newMessage.type !== "private_message") || !participants.find(p => p.name === newMessage.from)) {
         res.sendStatus(400);
     }
     else {
+        messages.push(newMessage);
         res.sendStatus(200);
     }
 });
+
+server.get(`/messages`, (req, res) => {
+    const userLoggedIn = req.headers.user;
+    const visibleMessages = messages.filter(m => m.from === userLoggedIn || m.to === userLoggedIn || m.to === "Todos" || m.type !== "private_message");
+    if(req.query.limit) {
+        res.send(visibleMessages.filter((m, i) => i<req.query.limit));
+    }
+    else {
+        res.send(visibleMessages);
+    }
+})
 
 server.listen(4000);
