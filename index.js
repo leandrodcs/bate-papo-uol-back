8b10bb4a9b62b939ff0dbdb94bdb70bee53afedb
leandrodcs <eng.leandrodcs@gmail.com>
2021-10-10 14:57:56 -0300

    Post route created to add new participants to the chat

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const server = express();
+server.use(cors());
+server.use(express.json());
 
-app.listen(4000);
+const participants = [
+    {
+        name: "Maria",
+        lastStatus: Date.now()
+    }
+];
+const messages = [];
+
+server.post(`/participants`, (req, res) => {
+    const newUser = {
+        ...req.body,
+        lastStatus: Date.now()
+    }
+    if(!newUser.name || participants.find((p) => p.name === newUser.name)) {
+        res.sendStatus(400);
+    }
+    else {
+        messages.push(
+            {
+                from: req.body.name,
+                to: "Todos",
+                text: "entra na sala...",
+                type: "status",
+                time: dayjs().format('hh:mm:ss')
+            }
+        )
+        res.status(200).send(messages);
+    }
+});
+
+server.listen(4000);
